d7f925c517f8d6c12f8f127df0d9efc81e3dcece
jesgarcia <jesgarjimher@gmail.com>
2022-11-29 23:08:52 +0000

    done

---
+++ largestNum.js
@@ -0,0 +1,15 @@
+/* jesus garcia
+lab 10 q4
+*/
+
+const arr = [-4,1,3,-100,5,7,9,22,66,77,99]
+
+for(let i = 0; i < arr.length; i++) {
+    var lgst = 0;
+    if(lgst < arr[i]){
+        lgst = arr[i]
+        
+    }
+}
+
+console.log("this is the largest " + lgst)
